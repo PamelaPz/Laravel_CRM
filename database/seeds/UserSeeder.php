23dfdb8720a1d92bf1e7407cd71df94f4685cf88
PamelaPz <jpamelapz02@gmail.com>
2020-06-05 16:58:30 -0500

    add more users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -9,36 +9,78 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Jessica',
-            'lastname' => 'Pérez',
+            'lastname' => 'Pérez Pérez',
             'email' => 'jesypame321@gmail.com',
             'password' => bcrypt('pamela'),
         ]);
 
         User::create([
             'name' => 'Pedro',
-            'lastname' => 'Juárez',
+            'lastname' => 'Juárez García',
             'email' => 'pedro22@gmail.com',
             'password' => bcrypt('pedro'),
         ]);
 
         User::create([
             'name' => 'Beatriz',
-            'lastname' => 'García',
+            'lastname' => 'García Pacheco',
             'email' => 'bety_garcía@gmail.com',
             'password' => bcrypt('bety'),
         ]);
 
         User::create([
             'name' => 'Julia',
-            'lastname' => 'López',
+            'lastname' => 'López Ruiz',
             'email' => 'juliyta_lopz@gmail.com',
             'password' => bcrypt('bety'),
         ]);
 
         User::create([
             'name' => 'Martín',
-            'lastname' => 'Sánchez',
+            'lastname' => 'Sánchez Contreras',
             'email' => 'martin.S88@gmail.com',
+            'password' => bcrypt('martin'),
+        ]);
+
+        User::create([
+            'name' => 'Sofia',
+            'lastname' => 'Jaramillo Figueroa',
+            'email' => 'sofi_jaramillo.f@gmail.com',
+            'password' => bcrypt('bety'),
+        ]);
+
+        User::create([
+            'name' => 'Stifer Yajaira',
+            'lastname' => 'Orduz Soto',
+            'email' => 'ortiz.stifer@gmail.com',
+            'password' => bcrypt('martin'),
+        ]);
+
+        User::create([
+            'name' => 'Estefanny',
+            'lastname' => 'Fonseca Cardenas',
+            'email' => 'stefanny.fonse55@gmail.com',
+            'password' => bcrypt('bety'),
+        ]);
+
+        User::create([
+            'name' => 'Dayanna',
+            'lastname' => 'Suárez Maldonado',
+            'email' => 'dayiana.ss09-mal@gmail.com',
+            'password' => bcrypt('martin'),
+        ]);
+
+        User::create([
+            'name' => 'Angie',
+            'lastname' => 'Delgado Almeyda',
+            'email' => 'delgado_angiee@gmail.com',
+            'password' => bcrypt('bety'),
+        ]);
+
+        User::create([
+            'name' => 'Yulieth Daniela',
+            'lastname' => 'Zabala Rodríguez',
+            'email' => 'yul.zabala2@gmail.com',
             'password' => bcrypt('martin'),
         ]);
     }
